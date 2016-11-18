2455333fe92f0f973e1459da136e3f2a869b9d6a
Paweł Płazieński <nivertius@gmail.com>
2016-11-18 15:47:22 +0100

    Dont write fan speed if it has not changed

--- driver.py
+++ driver.py
@@ -12,6 +12,7 @@ class Fan(Driver):
 	def __init__(self, mode_path, speed_path):
 		self._mode_path = mode_path
 		self._speed_path = speed_path
+		self._last_write = 0
 	
 	def __enter__(self):
 		self._mode_file = os.open(self._mode_path, os.O_WRONLY)
@@ -25,11 +26,14 @@ class Fan(Driver):
 		os.close(self._mode_file)
 
 	def update(self, control):
-		target = int(control * 255)
-		if target >= 255:
-			target = 254
-		elif target <= 3:
-			target = 2
+		if control >= 1.0:
+			control = 1.0
+		elif control <= 0.0:
+			control = 0.0
+		target = int(control * 251) + 3
+		if target == self._last_write:
+			return
 		written = str(target).encode()
 		Fan.LOGGER.debug("Fan set to %i", target)
 		os.write(self._speed_file, written)
+		self._last_write = target
